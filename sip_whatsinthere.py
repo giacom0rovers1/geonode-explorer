84abc8ccde9c5f0efbdcd947d64fa990d0fb4f28
Giacomo Roversi <giacomo.roversi@edu.unife.it>
2023-07-04 19:11:47 +0200

    init (code)

---
+++ sip_whatsinthere.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 23 18:12:07 2023
+
+@author: giacom0rovers1
+"""
+# import os
+import requests
+import warnings
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# empty lists
+pk = []
+uuid = []
+title = []
+author = []
+rtype = []
+lic = []
+cdate = []
+ldate = []
+abst = []
+alt = []
+
+
+# TODO: cross check con le categorie di geonode (api/v2/categories)
+
+url = "https://platform.score-eu-project.eu/api/v2/resources"
+
+# params = {"Authorization" : "Bearer UjYxYUKtEoDg3ifrucJNnMEr8djazr", "format" : "json"}
+# page = 1
+# page_size = 10
+# total = 132
+
+while url != None:
+    response = requests.get(url)
+    
+    if response.status_code == 200:     
+    
+        # GET data
+        data = response.json()
+        
+        print(f"Page {data.get('page')}  [page size: {data.get('page_size')}, total: {data.get('total')}]")
+    
+        # next page
+        url = data["links"].get("next")
+        
+    
+        for id in range(0, data["page_size"]-1):
+            
+            # select one resource
+            res = data["resources"][id]
+            
+            # identifier
+            pk.append(res.get("pk"))
+            
+            # uuid
+            uuid.append(res.get("uuid"))
+            
+            # title
+            title.append(res.get("title"))
+            
+               
+            # author [username (full name)]
+            name_fields = ["owner", "metadata_author", "poc"]
+            str = []
+            for field in name_fields:
+                str.append(res[field].get("username") + " (" +
+                           res[field].get("first_name") + " " + 
+                           res[field].get("last_name") + ")")
+            if not str[0] == str[1] == str[2]:
+                warnings.warn("Warning: more than one person involved...")
+            author.append(str[0]) # select owner only (first entry)
+            
+            # data type
+            rtype.append(res.get("resource_type"))
+            
+            # license
+            lic.append(res["license"].get("identifier"))
+            
+            
+            # creation date
+            cdate.append(res.get("created"))
+            
+            # last updated date
+            ldate.append(res.get("last_updated"))
+            
+            # abstract
+            abst.append(res.get("abstract"))
+            
+            # alternate text
+            alt.append(res.get("alternate"))    
+            
+            
+            # > For loop ends here._
+    else:
+        print(url)
+        print(f"Error: {response.status_code}")
+        break
+    
+    # > While loop ends here._
+
+
+d = {"PK" :             pk,
+     "UUID" :           uuid,
+     "Title" :          title,
+     "Author" :         author, 
+     "Type" :           rtype, 
+     "License" :        lic,
+     "Created" :        cdate,
+     "Last updated" :   ldate,
+     "Abstract" :       abst,
+     "Alt-name" :       alt}
+
+df = pd.DataFrame(data=d)
+
+# Dates as dates
+for key in ["Created", "Last updated"]:
+    df[key] = df[key].astype("datetime64[ns]")
+
+# Categorical
+for key in ["Author", "Type", "License"]:
+    df[key] = df[key].astype("category")
+
+print(df)
+df.to_csv('SIP_table.csv')
+
+
+len(set(df["Author"]))
+
+
+# Exploratory graphs
+
+df["Author"].value_counts().plot(kind='bar', ylabel='Count')
+df["Author"].value_counts().plot(kind='pie', explode = (0.1, 0, 0, 0, 0, 0, 0, 0), autopct='%1.1f%%')
+
+
+df["Type"].value_counts().plot(kind='pie', explode = (0, 0, 0.1, 0), autopct='%1.1f%%')
+
+df["License"].value_counts().plot(kind='bar')
+
+# Spunti...
+# https://medium.com/@kvnamipara/a-better-visualisation-of-pie-charts-by-matplotlib-935b7667d77f
+
+# # Authors
+# fig01, ax01 = plt.subplots()
+# ax01.hist(df["Author"])
+# plt.show()
+
+
+
+# Time serie of creation dates (increase number of resources..)
+df["Created"].plot()
+df["Last updated"].plot()
+
+
+
+# df.groupby(df["Created"].dt.month).count().plot(kind="bar")
+
+
+
+# > Completed._
+
+
+
+# Drafts
+
+# keys = ["first_name", "last_name"] #, "username"]
+# [data["resources"][0]["owner"].get(key) for key in keys]
+
+# data["resources"][0]["owner"].items()
+
+
