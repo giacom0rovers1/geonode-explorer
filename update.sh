4f58b53dd062a68d1e4c827a80c4bc3890557d4e
giacom0rovers1 <g.roversi@isac.cnr.it>
2024-07-31 16:37:57 +0200

    nbconvert + git updater

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ----------------------------------------
+# SCORE Project - GeoNode explorer updater
+# Giacomo Roversi
+# CNR-ISAC Rome (IT)
+# 31 July 2024
+# ----------------------------------------
+
+#SBATCH -n 1
+#SBATCH --mem=2G
+#SBATCH --time 2:00:00
+#SBATCH --mail-type=end
+#SBATCH --mail-user=g.roversi@isac.cnr.it
+#SBATCH --output=/work/users/roversi/logs/%j_%x.log
+
+# Execute the notebook
+jupyter nbconvert --to notebook --execute --inplace notebook.ipynb
+
+# Upload the changes
+git commit -am "Monthly automatic update"
+git push
+
+# Done.
+
+
