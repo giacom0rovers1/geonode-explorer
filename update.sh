d895e861c8fc85bbd078f514527b10564a684f6b
giacom0rovers1 <g.roversi@isac.cnr.it>
2025-02-08 20:14:10 +0100

    Monthly automatic update

--- update.sh
+++ update.sh
@@ -12,17 +12,21 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-user=g.roversi@isac.cnr.it
 #SBATCH --output=/home/roversi/work/logs/%j_%x.log
+#SBATCH --error=/home/roversi/work/logs/%j_%x.log
+
 
 export DISPLAY=:0
 
 cd "$HOME/geonode-explorer/"
 
 # Activate the conda environment for the crontab shell
-source "$HOME/anaconda3/etc/profile.d/conda.sh"
+source $HOME/anaconda3/etc/profile.d/conda.sh
 conda activate
 
 # Execute the notebook with extended timeout (10 min)
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to notebook --execute --inplace notebook.ipynb
+#conda run -n base jupyter nbconvert --ExecutePreprocessor.timeout=600 --to notebook --execute --inplace notebook.ipynb
+
 
 # Upload the changes
 git commit -am "Monthly automatic update"
